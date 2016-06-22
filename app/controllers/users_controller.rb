16263f0fc8351add7c16f6252ee800c622931930
Heber Sandoval <he3ers@gmail.com>
2016-06-22 11:21:55 -0400

    Create signup form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user # the user is logged in upon signup
+      flash[:success] = "Welcome, your account has been created."
+      redirect_to @user
+    else
+      flash.now[:danger] = "Your account has not been created."
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
